45ce206c9fc2f0457749f1278a8e014a3ba2533b
Molly Rodil <molly.rodil@happydojo.com>
2016-07-12 18:28:43 -0700

    Added moving the camera relative to the player, and added walls to the play area.

---
+++ Roll A Ball/Assets/scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start() 
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate runs every frame like Update but is 
+	//guaranteed to run after all items have been processed and updated.
+	void LateUpdate() 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
